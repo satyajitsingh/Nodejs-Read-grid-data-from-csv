1d380600afce125bdf7ab772c8c582fa63efdf77
Satyajit Singh <satyajit_singh@yahoo.com>
2018-07-12 13:50:17 +0100

    updated test.js

--- common/services/test.js
+++ common/services/test.js
@@ -1,2 +1,8 @@
-var ss = require('../services/querycsv')
-ss.find(51.0216,-3.8749)
+const query = require('../services/querycsv')
+const data = query.find(51.0216,-3.8749)
+var assert = require('assert');
+describe('find', function() {
+    it('should return a JSON object if the correct parameters are p[assed to it', function () {
+        assert((query.find(51.0216,-3.8749)!= null)) ;
+    });
+})
